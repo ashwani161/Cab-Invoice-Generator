e60851de2e1b4054cd005dff232c43fe97ad9367
ashwani161 <ashwanin65@gmail.com>
2021-04-08 11:23:52 +0530

    Added:To find out the TotalFare and Minfare by given data

--- src/main/java/CabInvoice/InvoiceGenerator.java
+++ src/main/java/CabInvoice/InvoiceGenerator.java
@@ -1,7 +1,19 @@
 package CabInvoice;
 
 public class InvoiceGenerator {
-public static void main(String[] args) {
-	System.out.println("Welcome to Cab Invoice Generator");
-}
+	private static final double MINIMUN_COST_PER_KILOMETER =10;
+	private static final int COST_PER_TIME = 1;
+	private static final double MINIMUN_FARE = 5;
+
+	public double calculateFare(double distance, int time) {
+		double totalFare =  distance * MINIMUN_COST_PER_KILOMETER  + time * COST_PER_TIME ;
+		if(totalFare < MINIMUN_FARE )
+			return  MINIMUN_FARE;
+		return totalFare;
+	}
+//public static void main(String[] args) {
+//	System.out.println("Welcome to Cab Invoice Generator");
+//}
+
+
 }
---
+++ src/main/java/CabInvoice/InvoiceServiceTest.java
@@ -0,0 +1,26 @@
+package CabInvoice;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InvoiceServiceTest {
+	@Test
+	public void givenDistanceAndTime_ShouldReturnTotalFare() {
+		InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
+		double distance = 2.0;
+		int time = 5;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(25, fare, 0.0);
+	}
+	
+	@Test
+	public void givenDistanceAndTime_ShouldReturnMinFare() {
+		InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
+		double distance = 0.2;
+		int time = 1;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(5, fare, 0.0);
+	}
+
+
+}
