8833f21620c09f57871c97ef3a9a871cfd7ea6e7
ashwani161 <ashwanin65@gmail.com>
2021-04-08 21:29:06 +0530

    Added:NewRiderepository and return invoice by Userid

--- src/main/java/CabInvoice/InvoiceGenerator.java
+++ src/main/java/CabInvoice/InvoiceGenerator.java
@@ -1,5 +1,7 @@
 package CabInvoice;
 
+import java.util.ArrayList;
+
 public class InvoiceGenerator {
 	private static final double MINIMUN_COST_PER_KILOMETER =10;
 	private static final int COST_PER_TIME = 1;
@@ -16,12 +18,12 @@ public class InvoiceGenerator {
 //	System.out.println("Welcome to Cab Invoice Generator");
 //}
 
-	public InvoiceSummary calculateFare(Ride[] rides) {
+	public InvoiceSummary calculateFare(ArrayList<Ride> rides) {
 		double totalFare = 0;
 		for (Ride ride:rides) {
 			totalFare += this.calculateFare(ride.distance, ride.time);
 		}
-		return new InvoiceSummary(rides.length, totalFare);
+		return new InvoiceSummary(rides.size(), totalFare);
 	}
 
 
--- src/main/java/CabInvoice/InvoiceServiceTest.java
+++ src/main/java/CabInvoice/InvoiceServiceTest.java
@@ -1,5 +1,7 @@
 package CabInvoice;
 
+import java.util.ArrayList;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -25,10 +27,22 @@ public class InvoiceServiceTest {
 	@Test
 	public void givenMultipleRides_ShouldReturnInvoiceSummary() {
 		InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
-        Ride[] rides = {new Ride(2.0, 5),
-        		         new Ride(0.1, 1)
-                       };
+        ArrayList<Ride> rides = new ArrayList<Ride>();
+        		rides.add(new Ride(2.0, 5));
+        		rides.add(new Ride(0.1, 1));
         InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+        InvoiceSummary exceptedinvoiceSummary = new InvoiceSummary(2, 30.0);
+		Assert.assertEquals(exceptedinvoiceSummary, summary);
+
+     }
+	
+	@Test
+	public void givenMultipleRides_ShouldReturnInvoiceSummary1() {
+		InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
+		RideRepository riderepo = new RideRepository();
+		riderepo.addRidetoUser("User", new Ride(2.0, 5));
+		riderepo.addRidetoUser("User", new Ride(0.1, 1));
+        InvoiceSummary summary = invoiceGenerator.calculateFare(riderepo.getRides("User"));
         InvoiceSummary exceptedinvoiceSummary = new InvoiceSummary(2, 30.0);
 		Assert.assertEquals(exceptedinvoiceSummary, summary);
 
