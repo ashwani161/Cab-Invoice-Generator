44b21ba696c5d0892a3b4df57be4a011dfc36baa
ashwani161 <ashwanin65@gmail.com>
2021-04-08 21:41:34 +0530

    Modified:Cab Invoice has 2 catagories Normal and Premium and Find MinimumFare

--- src/main/java/CabInvoice/InvoiceServiceTest.java
+++ src/main/java/CabInvoice/InvoiceServiceTest.java
@@ -3,12 +3,16 @@ package CabInvoice;
 import java.util.ArrayList;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
+	InvoiceGenerator invoiceGenerator = null;
+	@Before void setup() throws Exception {
+		InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
+	}
 	@Test
 	public void givenDistanceAndTime_ShouldReturnTotalFare() {
-		InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
 		double distance = 2.0;
 		int time = 5;
 		double fare = invoiceGenerator.calculateFare(distance, time);
@@ -17,7 +21,6 @@ public class InvoiceServiceTest {
 	
 	@Test
 	public void givenDistanceAndTime_ShouldReturnMinFare() {
-		InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
 		double distance = 0.2;
 		int time = 1;
 		double fare = invoiceGenerator.calculateFare(distance, time);
@@ -26,7 +29,6 @@ public class InvoiceServiceTest {
 	
 	@Test
 	public void givenMultipleRides_ShouldReturnInvoiceSummary() {
-		InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
         ArrayList<Ride> rides = new ArrayList<Ride>();
         		rides.add(new Ride(2.0, 5));
         		rides.add(new Ride(0.1, 1));
@@ -38,7 +40,6 @@ public class InvoiceServiceTest {
 	
 	@Test
 	public void givenMultipleRides_ShouldReturnInvoiceSummary1() {
-		InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
 		RideRepository riderepo = new RideRepository();
 		riderepo.addRidetoUser("User", new Ride(2.0, 5));
 		riderepo.addRidetoUser("User", new Ride(0.1, 1));
@@ -47,4 +48,20 @@ public class InvoiceServiceTest {
 		Assert.assertEquals(exceptedinvoiceSummary, summary);
 
      }
+	
+	@Test
+	public void givenDistanceAndTime_ShouldReturnPremiumTotalFare() {
+		double distance = 2.0;
+		int time = 5;
+		double fare = invoiceGenerator.calculateFare1(distance, time);
+		Assert.assertEquals(40, fare, 0.0);
+	}
+	
+	@Test
+	public void givenDistanceAndTime_ShouldReturnpremiumMinFare() {
+		double distance = 0.5;
+		int time = 4;
+		double fare = invoiceGenerator.calculateFare1(distance, time);
+		Assert.assertEquals(20, fare, 0.0);
+	}
 }
